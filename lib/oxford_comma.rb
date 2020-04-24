1c732011cd3cdd53b4c515b677a1aeb114a575d0
Cody Dupuis <codydupuis99@gmail.com>
2020-04-24 19:41:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
 def oxford_comma(array)
+array = ["kiwi", "strawberry", "apple", "orange"]
 
-end 
-  if array.length == 1
+def oxford_comma(array)
+  if array.size == 1
     array.join
-  elsif array.length == 2
-    array.join(" and ")
-  elsif array.length >= 3
-    array[-1].insert(0, "and ")
-    array.join(", ")
+    elsif array.size == 2
+      array.join(" and ")
+  else
+    array[0..-2].join(", ") + ", and " + array[-1]
   end
- end
+end 
